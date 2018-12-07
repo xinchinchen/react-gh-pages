9da9b2388be7ccb6e15ce6eeda07b6f9b72b2a29
xinchinchen <xinchinchen@gmail.com>
2018-12-08 00:21:25 +0800

    Updates

---
+++ precache-manifest.8580a05b91977dc6cb7b347efc210146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "77dcef70020bbc985f7d",
+    "url": "/react-gh-pages/static/css/main.db485b7a.chunk.css"
+  },
+  {
+    "revision": "77dcef70020bbc985f7d",
+    "url": "/react-gh-pages/static/js/main.77dcef70.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/react-gh-pages/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c0e228a1a417772378bbfda737b2ad68",
+    "url": "/react-gh-pages/index.html"
+  }
+];
